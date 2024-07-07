93a97c1e68c48e8eacdff7e005c9529d3e3bc5fc
devmanso <mansourquddus@protonmail.com>
2024-07-07 00:48:44 -0400

    add death/respwan mechanic

--- Scripts/DeadZone.gd
+++ Scripts/DeadZone.gd
@@ -3,6 +3,7 @@ extends Area3D
 # ---------- VARIABLES ---------- #
 
 @onready var spawn_position = %SpawnPosition
+
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 # ---------- SIGNALS ---------- #
@@ -11,3 +12,4 @@ func _on_body_entered(body):
 	# Checks if player is colliding with the dead zone & reset player position if true
 	if body.is_in_group("Player"):
 		player.global_position = spawn_position.global_position
+		GameManager.add_death()
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,17 +2,26 @@ extends Node3D
 
 # ---------- VARIABLES ---------- #
 
-var score = 0
+var score : int = 0
+var death_count : int = 0
 
 # ---------- FUNCTIONS ---------- #
 
 func _process(_delta):
 	show_mouse_cursor()
+	if death_count == 3:
+		death_count = 0
+		get_tree().reload_current_scene()
 
 # Making Cursor visible using "mouse_visible" key which is assigned in Project Settings > Input Map
 func show_mouse_cursor():
 	if Input.is_action_just_pressed("mouse_visible"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	elif Input.is_action_just_pressed("mouse_recapture"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func add_score():
 	score += 1
+
+func add_death():
+	death_count += 1
